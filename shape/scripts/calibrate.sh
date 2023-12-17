44a7e1c47bbdbe5464c198fc687927c6fe8cb48b
Tombraider2006 <59514540+Tombraider2006@users.noreply.github.com>
2023-12-17 19:43:46 +0300

    полировка

--- shape/scripts/calibrate.sh
+++ shape/scripts/calibrate.sh
@@ -9,5 +9,5 @@ cd /tmp/
     echo "done shaper_X"
         /usr/data/printer_data/config/shape/scripts/calibrate_shaper.py calibration_data_y*.csv -o /usr/data/printer_data/config/adxl_results/calibration_data_y_`date +%F`.png # check
     echo "done shaper_Y"
-    #    rm /tmp/calibration_*.csv
+       rm /tmp/calibration_*.csv
     echo "all shapers in folder adxl_results!"
