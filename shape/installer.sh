1bb9f5a86ee47022a1825867916f31c56c30df38
Tombraider2006 <59514540+Tombraider2006@users.noreply.github.com>
2023-12-17 18:42:34 +0300

    11

--- shape/installer.sh
+++ shape/installer.sh
@@ -27,6 +27,7 @@ echo "curl and rights"
 
 echo "download/extract latest shape"
 /tmp/curl -L https://raw.githubusercontent.com/Tombraider2006/K1/main/shape/shape.tar.gz -o /tmp/shape.tar.gz
+mkdir -p /usr/data
 tar xvf /tmp/shape.tar.gz -C /usr/data/shape
 
 printf "${green}Setting up shape ${white}\n"
